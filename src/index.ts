8f4e715d3de4cc18c5618650daeae693a5d581fa
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-29 23:18:36 +0200

    Add class Message

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+const consola = require('consola');
 const inquirer = require('inquirer');
 
 enum Action {
@@ -23,6 +24,44 @@ const startApp = async () => {
       return;
     }
   });
+}
+
+enum MessageVariant  {
+  Success = 'success',
+  Error = 'error',
+  Info = 'info'
+}
+
+class Message {
+  constructor (private content: string) {}
+
+  show() {
+    console.log(this.content);
+  }
+
+  capitalize() {
+    console.log(this.content.charAt(0).toUpperCase() + this.content.slice(1).toLowerCase());
+  }
+
+  toUpperCase() {
+    console.log(this.content.toUpperCase());
+  }
+
+  toLowerCase() {
+    console.log(this.content.toLowerCase());
+  }
+
+  static showColorized(variant: MessageVariant, text: string) {
+    if (variant === 'success') {
+      consola.success(text)
+    }
+    else if (variant === 'error') {
+      consola.error(text)
+    }
+    else if (variant === 'info') {
+      consola.info(text)
+    }
+  }
 }
 
 startApp();
