6f7c0090226d1b1e7a71d33260348032ab9e161c
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-29 21:40:51 +0200

    Add type for answers in startApp

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 const inquirer = require('inquirer');
 
-const startApp = () => {
+enum Action {
+  List = 'list',
+  Add = 'add',
+  Remove = 'remove',
+  Quit = 'quit'
+}
+
+type InquirerAnswers = {
+  action: Action
+}
+
+const startApp = async () => {
   inquirer.prompt([{
     name: 'action',
     type: 'input',
     message: 'How can I help you?',
-  }]).then((answers: { action: 'list' | 'add' | 'remove' | 'quit' }) => {
+  }]).then((answers: InquirerAnswers) => {
     console.log("Chosen action: " + answers.action);
     startApp();
     if (answers.action === "quit") {
